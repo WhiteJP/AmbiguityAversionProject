071d5e6af5301b5372396f2e29fe2674878e2f1e
Josh White <joshua.white@outlook.com.au>
2019-03-25 22:09:21 +1100

    minor updates

--- experiment/data/Pilotdatacheck.R
+++ experiment/data/Pilotdatacheck.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(here)
-library(car)
+library(car) 
+library(MCMCglmm)
+library(ordinal)
 
 # set wd
 loc <- here("experiment", "data")
@@ -36,4 +38,24 @@ d
 
 #quick graph
 ggplot(d, aes(x = vignette, y = vignetteAnswer, fill = vignetteType)) + geom_bar(stat = "identity") + ylim(c(-3, 3))
+
+
+
+#####FIT ORDINAL LOGISTIC MIXED EFFECTS MODEL#####
+ #recode dep variable to ordered factor
+d$vignetteAnswer<- ordered(d$vignetteAnswer, levels = c("-3", "-2", "-1", "0", "1", "2", "3"))
+d$nopred <- 1
+
+  # frequentist - using clmm (cumulative link mixed model) function from ordinal package for mixed models, but 
+  #Polr (proportional odds logistic regression) function from MASS when no random effects (clmm doesn't work here)
+    olm.freq.nopred <-  polr(vignetteAnswer ~ nopred, data = d, method = "logistic") 
+    olm.freq.GLpred <-  polr(vignetteAnswer ~ vignetteType, data = d, method = "logistic") 
+    olm.freq.ordpred <- polr(vignetteAnswer ~ answerOrder, data = d, method = "logistic") 
+    olmm.freq <-  clmm(vignetteAnswer ~ vignetteType + (1|vignette), data = d, link = "logit", nAGQ = 10)
+   
+  # Bayesian - using brms package
+    olm.bayes.nopred <-  brm(vignetteAnswer ~ nopred, data = d, family = cumulative(link= "logit")) 
+    olm.bayes.GLpred <-  brm(vignetteAnswer ~ vignetteType, data = d, family = cumulative(link= "logit")) 
+    olm.bayes.ordpred <- brm(vignetteAnswer ~ answerOrder, data = d, family = cumulative(link= "logit")) 
+    olmm.bayes <-   brm(vignetteAnswer ~ vignetteType + (1|vignette), data = d, family = cumulative(link= "logit")) 
 
