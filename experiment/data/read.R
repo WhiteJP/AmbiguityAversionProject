a46f4ad94fad6d89f4644e9815ed1ae23f61d923
Josh White <joshua.white@outlook.com.au>
2019-05-02 11:14:56 +1000

    adding final analysis of data, and slides for lab presentation

--- experiment/data/read.R
+++ experiment/data/read.R
@@ -3,12 +3,12 @@ location <- here("experiment", "data")
 setwd(location)
 
 # read in turk data from gae export
-rawData <- read.csv(file="rawresults3.csv", header=TRUE)
+rawData <- read.csv(file="rawresults5.csv", header=TRUE)
 
 source('JSON_parser_function.R')
 # this takes 10+ minutes
 d <- parseJSONlist(rawData[,1])
-write.csv(d,file="parsedrawresults1.csv")
+write.csv(d,file="parsedrawresults.csv")
 
 
 
