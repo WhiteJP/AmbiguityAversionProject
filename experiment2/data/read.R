5e3ade5dd7c82995e6e41ddcfde80a6dabb56e59
WhiteJP <joshua.white@outlook.com.au>
2020-02-07 22:22:06 +1100

    add analysis of experiment 2

--- experiment2/data/read.R
+++ experiment2/data/read.R
@@ -3,12 +3,12 @@ location <- here("experiment2", "data")
 setwd(location)
 
 # read in turk data from gae export
-rawData <- read.csv(file="rawresults.csv", header=TRUE)
+rawData <- read.csv(file="rawresultsexp2.csv", header=TRUE)
 
 source('JSON_parser_function.R')
 # this takes 10+ minutes
 d <- parseJSONlist(rawData[,1])
-write.csv(d,file="parsedrawresults.csv")
+write.csv(d,file="parsedrawresultsexp2.csv")
 
 
 
